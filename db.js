cb816a93c1cd9815029cf9350756a1d0a42fc183
hamzakhan997 <hamzakhaniazi@gmail.com>
2022-10-30 00:56:22 +0500

    created table on heroku

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const { Model, knexSnakeCaseMappers } = require('objection');
 
 const db = require('knex')({
-    client: 'mysql',
+    client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'turing',
+        host: 'ec2-52-70-45-163.compute-1.amazonaws.com',
+        user: 'ywizhgfyskbofw',
+        password: '6b387c3d8484095a4976bed7266026e5404dfddd870f8d5b8b73e3a25a318f1e',
+        database: 'ddn1nd5dpelmv3',
+        port: 5432,
+        ssl: { rejectUnauthorized: false },
     },
     pool: {
         min: 0,
